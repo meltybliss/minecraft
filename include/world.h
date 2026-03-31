5c15279668ef1ed0e0e0e97750793ed543f303ec
melty <Yusei.k.0403@gmail.com>
2026-03-31 21:17:25 +0900

    made world

---
+++ include/world.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "chunk.h"
+#include <memory>
+#include <unordered_map>
+#include <stdint.h>
+class World {
+public:
+	World();
+
+	void Tick();
+	void render();
+
+private:
+
+	int RENDER_DISTANCE = 2;
+	int UNLOAD_DISTANCE = 4;
+
+	std::unordered_map<uint64_t, std::unique_ptr<Chunk>> Chunks;
+
+	uint64_t GetChunkKey(int32_t cx, int32_t cz) const {
+		return (static_cast<uint64_t>(static_cast<uint32_t>(cx)) << 32 | static_cast<uint32_t>(cz));
+	}
+
+};
